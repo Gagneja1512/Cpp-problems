476f1a522f80c433d849bfd62df2dd8c37833dfe
gagneja1512 <aadityaarora1215@gmail.com>
2022-07-15 11:43:01 +0530

    Median Minimisation

---
+++ Codechef/medianMinimisation.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define max(a, b) (a < b ? b : a)
+#define min(a, b) ((a > b) ? b : a)
+#define mod 1e9 + 7
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+#define FORL(a, b, c) for (int(a) = (b); (a) <= (c); (a)++)
+#define FORR(a, b, c) for (int(a) = (b); (a) >= (c); (a)--)
+typedef long long int ll;
+#define f first
+#define s second
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define endl '\n'
+#define vl vector<long long>
+
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while (T--) {
+    	ll n;cin>>n;
+
+    	vl a(n);
+    	for(int i=0 ; i<n ; i++)
+    		cin>>a[i];
+
+    	sort(a.begin() , a.end());
+
+    	cout<<a[n/2] - a[n/2-1]<<endl;
+    }
+    return 0;
+}
